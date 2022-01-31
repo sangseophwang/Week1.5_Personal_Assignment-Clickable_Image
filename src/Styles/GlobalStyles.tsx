33f3519064c223fe4b66da2a886cc517830cca2c
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-01-31 21:57:11 +0900

    modify: 웹폰트 위치 이동

--- src/Styles/GlobalStyles.tsx
+++ src/Styles/GlobalStyles.tsx
@@ -3,10 +3,9 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
   ${reset};
-  @import url('https://spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css');
   body {
-    font-family: 'Spoqa Han Sans Neo', 'sans-serif';
-  }
+        font-family: "Spoqa Han Sans Neo", "sans-serif";
+      }
 `;
 
 export default GlobalStyles;
