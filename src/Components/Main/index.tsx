aeddb87981cd7e0cac35fc8464932ad1e661d6fd
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-02-01 20:26:21 +0900

    feat: 메인 컨텐츠 컴포넌트 구현

--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -1,7 +1,39 @@
+import styled from "styled-components";
+import useProduct from "Hooks/useProduct";
+import Title from "Components/Main/Title";
+import Contents from "Components/Main/Contents";
+
 export default function Main() {
-  console.log(
-    "%c집꾸미기 프로젝트 by 황상섭",
-    "text-shadow: 2px 2px 1px rgba(255,121,11,0.5), -2px -2px 1px rgba(125,66,234,0.5);color: rgba(0,0,0,0.6); font-size: 40px; font-weight: bold"
+  const { data, loading, error } = useProduct();
+
+  if (loading) {
+    return <div>로딩 중입니다.</div>;
+  }
+  if (error) {
+    return (
+      <div>
+        데이터가 존재하지 않거나 불러오기에 실패했습니다. 다시 시도해주시기
+        바랍니다.
+      </div>
+    );
+  }
+
+  return (
+    <Container>
+      <Wrapper>
+        <Title />
+        <Contents data={data} />
+      </Wrapper>
+    </Container>
   );
-  return <div></div>;
 }
+
+const Container = styled.main`
+  padding: 50px 0;
+  display: flex;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 800px;
+`;
