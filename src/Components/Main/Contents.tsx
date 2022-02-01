0e44d57d7c93a36ee1723e2578f8607acf40108c
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-02-01 20:27:03 +0900

    feat: 이미지 전체 컴포넌트 구현

---
+++ src/Components/Main/Contents.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { ProductInterface } from "Types";
+import styled from "styled-components";
+import ImagePointList from "Components/Main/ImagePointList";
+import SubImageList from "Components/Main/SubImageList";
+
+interface ProductProps {
+  data: ProductInterface;
+}
+
+export default function Contents({ data }: ProductProps) {
+  const { imageUrl, productList } = data;
+  const [selected, setSelected] = useState<number | null>(null);
+
+  const onSelectImage = (id: number | null) => setSelected(id);
+
+  return (
+    <Container>
+      <ImageWrapper>
+        <MainImage src={imageUrl} alt="메인 이미지" />
+        <ImagePointList
+          productList={productList}
+          selectedImage={selected}
+          onSelectImage={onSelectImage}
+        />
+      </ImageWrapper>
+      <SubImageList
+        productList={productList}
+        selectedImage={selected}
+        onSelectImage={onSelectImage}
+      />
+    </Container>
+  );
+}
+
+const Container = styled.article``;
+
+const ImageWrapper = styled.div`
+  position: relative;
+`;
+
+const MainImage = styled.img`
+  width: 100%;
+  cursor: pointer;
+  height: auto;
+`;
